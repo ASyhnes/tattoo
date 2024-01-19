c8128a29d0902df8b486bc3e330b230eb246242c
ASyhnes <syhnes@gmail.com>
2024-01-19 08:33:52 +0100

    travail sur la page flash

--- thistleskull.php
+++ thistleskull.php
@@ -6,43 +6,51 @@
     <link rel="stylesheet" href="footer.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="navbar.css">
-    <link rel="stylesheet" href="tattoopresentation.css">
     <link rel="stylesheet" href="dex.css">
+    <link rel="stylesheet" href="bouton.css">
+    <link rel="stylesheet" href="tattoopresentation.css">
 </head>
+
 <body>
+    
 
-<?php
-  require_once(__DIR__ . '/navbar.php');
-?>
+    <?php
+    require_once(__DIR__ . '/navbar.php');
+    ?>
 
-<div class="info-box">
-  <h2>Thistel Skull</h2>
-  <br>
-  <p><strong>Taille Min:</strong> 15cm</p>
-  <p><strong>Taille Max :</strong> 30cm</p>
-  <br><br>
-  
-  <p><strong>Lore :</strong></p>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quasi eligendi expedita molestias ipsa officia sed excepturi id eius voluptatibus sequi fugit similique perferendis eveniet, et cumque veniam blanditiis quis!</p>
-  <br><br>
-  
-  <p><strong>Prix :</strong></p>
-  <br><br>
-  <p>A venir: slide variation pri en fonction de la taille</p>
-  <div class="slidecontainer">
-    <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
-    <p>Valeur : <span id="demo"></span></p>
-  </div>
-</div>
-
-Note: prvoir une section "Déja pris par XXXX"
-
-<?php
-  require_once(__DIR__ . '/footer.php');
-?>
+    <div class="info-box">
+    <h2>Thistel Skull</h2>
+    <br>
+    <p><strong>Taille Min:</strong> 15cm</p>
+    <p><strong>Taille Max :</strong> 30cm</p>
+    <br><br>
+    
+    <p><strong>Lore :</strong></p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quasi eligendi expedita molestias ipsa officia sed excepturi id eius voluptatibus sequi fugit similique perferendis eveniet, et cumque veniam blanditiis quis!</p>
+    <br><br>
+    
+    <p><strong>Prix :</strong></p>
+    <br><br>
+    <p>A venir: slide variation pri en fonction de la taille</p>
+    <div class="slidecontainer">
+        <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+        <p>Valeur : <span id="demo"></span></p>
+    </div>
+    <p>Note: prvoir une section "Déja pris par X....."</P>
+    </div>
 
 
-<script src="slider.js"></script>
+    <div class="flash-container">
+        <img src="img/Flashs/Skull Chardon.jpg" alt="Skull Chardon">
+    </div>
+
+
+    <?php
+    require_once(__DIR__ . '/generalfooter.php');
+    ?>
+
+
+    <script src="slider.js"></script>
 
 </body>
 </html>

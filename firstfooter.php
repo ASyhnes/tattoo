061e60e25cf3147a9742d26d031bd0339c25d81b
ASyhnes <syhnes@gmail.com>
2024-02-10 16:58:21 +0100

    MAJ boutons

--- firstfooter.php
+++ firstfooter.php
@@ -1,8 +1,10 @@
 <!--Firstfooter-->
 
 <footer>
-  <a href="#flashs">
-  <button class='glowing-btn'><span class='glowing-txt'>F<span class='faulty-letter'></span>la<span class='faulty-letter'>shs</span></span></button></a>
-  <a href="#about"><button class='glowing-btn'><span class='glowing-txt'>A<span class='faulty-letter'>b</span><span class='faulty-letter'>t M</span><span class='faulty-letter'>e</span></span></button></a>
-  <a href="#events"><button class='glowing-btn'><span class='glowing-txt'>E<span class='faulty-letter'>v</span><span class='faulty-letter'>nt</span><span class='faulty-letter'>s</span></span></button></a>
+  <div class="buttons-container">
+    <a href="#flashs">
+      <button class='glowing-btn'><span class='glowing-txt'>F<span class='faulty-letter'></span>la<span class='faulty-letter'>shs</span></span></button></a>
+      <a href="#about"><button class='glowing-btn'><span class='glowing-txt'>A<span class='faulty-letter'>b</span><span class='faulty-letter'>t M</span><span class='faulty-letter'>e</span></span></button></a>
+      <a href="#events"><button class='glowing-btn'><span class='glowing-txt'>E<span class='faulty-letter'>v</span><span class='faulty-letter'>nt</span><span class='faulty-letter'>s</span></span></button></a>
+  </div>
 </footer>

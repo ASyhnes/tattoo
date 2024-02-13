c3063f34aeaca2b357f08a90c334e1419ce23923
ASyhnes <syhnes@gmail.com>
2024-02-13 13:25:12 +0100

    nav barre style fin

--- firstnavbar.php
+++ firstnavbar.php
@@ -1,74 +1,77 @@
 <!--navbar-->
-
 <nav>
-        <!-- légende: /.mot./ egal lien non défini -->
-          <div class="wrapper">
-            <div class="logo"><img src="img/dos_Mini.png" alt=""></a></div>
-            <input type="radio" name="slider" id="menu-btn">
-            <input type="radio" name="slider" id="close-btn">
-            <ul class="nav-links">
-              <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
-              <li><a href="#about">Home</a></li>
-              <li>
+    <!-- légende: /.mot./ égal lien non défini -->
+    <div class="wrapper">
+        <div class="logo">
+            <img src="img/dos_Mini.png" alt="">
+        </div>
+        <input type="radio" name="slider" id="menu-btn">
+        <input type="radio" name="slider" id="close-btn">
+        <ul class="nav-links">
+            <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
+            <li><a href="#about">Home</a></li>
+            <li>
                 <a href="#" class="desktop-item">Shop</a>
                 <input type="checkbox" id="showDrop">
                 <label for="showDrop" class="mobile-item">Dropdown Menu</label>
                 <ul class="drop-menu">
-                  <li><a href="#">Rubrique<br>à venir<br>...</a></li>  
+                    <li><a href="#">Rubrique<br>à venir<br>...</a></li>
                 </ul>
-                <li>
+            </li>
+            <li>
                 <a href="#" class="desktop-item">InkMe</a>
                 <input type="checkbox" id="showDrop">
                 <label for="showDrop" class="mobile-item">Dropdown Menu</label>
                 <ul class="drop-menu">
-                  <li><a href="submitproject.php">Vous avez un projet?</a></li>  
-                  <li><a href="#flashs">Flashs</a></li>
+                    <li><a href="submitproject.php">Vous avez un projet?</a></li>
+                    <li><a href="#flashs">Flashs</a></li>
                 </ul>
-              </li>
-              <!--si tu veux une grosse box lors du déroulé
-              <li>
-                <a href="#photos" class="desktop-item">photo</a>
+            </li>
+            <!-- Commentaire pour une éventuelle future section
+            <li>
+                <a href="#photos" class="desktop-item">Photo</a>
                 <input type="checkbox" id="showMega">
                 <label for="showMega" class="mobile-item">...</label>
                 <div class="mega-box">
-                  <div class="content">
-                    <div class="row">
-                      <img src="img/goldsansfond2.png" alt="">
+                    <div class="content">
+                        <div class="row">
+                            <img src="img/goldsansfond2.png" alt="">
+                        </div>
+                        <div class="row">
+                            <header>...</header>
+                            <ul class="mega-links">
+                                <li><a href="#">...</a></li>
+                                <li><a href="#">...</a></li>
+                                <li><a href="#">...</a></li>
+                                <li><a href="#">...</a></li>
+                            </ul>
+                        </div>
+                        <div class="row">
+                            <header>...</header>
+                            <ul class="mega-links">
+                                <li><a href="#">...</a></li>
+                                <li><a href="#">...</a></li>
+                                <li><a href="#">...</a></li>
+                                <li><a href="#">...</a></li>
+                            </ul>
+                        </div>
+                        <div class="row">
+                            <header>...</header>
+                            <ul class="mega-links">
+                                <li><a href="#">...</a></li>
+                                <li><a href="#">...</a></li>
+                                <li><a href="#">...</a></li>
+                                <li><a href="#">...</a></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="row">
-                      <header>...</header>
-                      <ul class="mega-links">
-                        <li><a href="#">...</a></li>
-                        <li><a href="#">...</a></li>
-                        <li><a href="#">...</a></li>
-                        <li><a href="#">...</a></li>
-                      </ul>
-                    </div>
-                    <div class="row">
-                      <header>...</header>
-                      <ul class="mega-links">
-                        <li><a href="#">...</a></li>
-                        <li><a href="#">...</a></li>
-                        <li><a href="#">...</a></li>
-                        <li><a href="#">...</a></li>
-                      </ul>
-                    </div>
-                    <div class="row">
-                      <header>...</header>
-                      <ul class="mega-links">
-                        <li><a href="#">...</a></li>
-                        <li><a href="#">...</a></li>
-                        <li><a href="#">...</a></li>
-                        <li><a href="#">...</a></li>
-                      </ul>
-                    </div>
-                  </div>
                 </div>
-              </li>-->
-              <li><a href="#photos">Photo</a></li>
-              <li><a href="#">News</a></li>
-            </ul>
-            <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
-            <div href="#" class="titre">David Chardon</div>
-          </div>
-    </nav>
+            </li>
+            -->
+            <li><a href="#photos">Photo</a></li>
+            <li><a href="#">News</a></li>
+        </ul>
+        <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
+        <div class="titre">David Chardon</div>
+    </div>
+</nav>
